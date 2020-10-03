2fd5fd503cf911ac20ae50c73aeea1d148d26fdc
Dreamifi <jonatan.melin@hotmail.com>
2020-10-04 01:26:01 +0200

    Changed sizetest so it only creates exe files that don't already exist.

--- engine/runSizeTest.bat
+++ engine/runSizeTest.bat
@@ -1,10 +1,13 @@
 cd ..
-rmdir /s /q sizeTest
+if not exist sizeTest (
 mkdir sizeTest
+)
 cd sizeTest
 for %%f in (.\..\engine\sizeTest\*) do (
+if not exist %%~nf.exe (
 pyinstaller %%~ff --distpath . --onefile -n %%~nf
 rmdir /s /q build
 del %%~nf.spec
+)
 )
 pause
--- engine/sizeTest/allskimageTest.py
+++
@@ -1,5 +0,0 @@
-import skimage
-from skimage import color
-from skimage import transform
-from skimage import filters
-from skimage import morphology
